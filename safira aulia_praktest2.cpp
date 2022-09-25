122f560921b3de8853624fabced041e8a5a4a73a
safiiraaulia <112611988+safiiraaulia@users.noreply.github.com>
2022-09-25 08:53:30 +0700

    Add files via upload

---
+++ safira aulia_praktest2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  char angka;
+  cin >> angka ;
+  switch(angka){
+    case'1':
+    cout<<"satu"<<endl;
+    break;
+    case'2':
+    cout<<"dua"<<endl;
+    break;
+    case'3':
+    cout <<"tiga"<<endl;
+    break;
+    default:
+    cout<<"bilangan lebih besar dari tiga"<<endl;
+    break;
+   }
+  
+  }
+  
